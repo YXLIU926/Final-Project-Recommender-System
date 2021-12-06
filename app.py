3d39354ec9478b5ccb7fd4203423a8b2a986472c
YXLIU926 <93688200+YXLIU926@users.noreply.github.com>
2021-12-06 14:34:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,11 @@ nltk.download('stopwords')
 stop_words = ""
 sub_replace = None
 
-
-
 def clean_txt(text):
     text.lower()
     text = sub_replace.sub('',text)
     ' '.join(word for word in text.split() if word not in stop_words)
     return text
-
 
 #########################################################################
 
@@ -87,7 +84,6 @@ def recommendations(name, hotel_class_required, cleanliness_required, sleep_qual
     return recommended_hotels
 
 
-
 @st.cache(allow_output_mutation=True)
 def load_model():
     global stop_words
@@ -97,20 +93,20 @@ def load_model():
     sub_replace = re.compile('[^0-9a-z #+_]')
     stop_words = set(stopwords.words('english'))
     print("Loading Data")
-    df = pd.read_csv(r'C:\Users\elton\Desktop\Kate_Project\code\clean_data.csv')
+    df = pd.read_csv(r'C:\Users\yunxinliu\Documents\GitHub\clean_data.csv')
     print(df.head())
     tempdf = df[['name', 'city']]
     for index, row in tempdf.iterrows():
         if row['city'] == "Seattle":
             seattleHotels.append(row['name'])
-    seattleHotels = seattleHotels.sort()
+    seattleHotels.sort()
 
     print("Calculating Word Total For Each Review")
     df['word_count']=df['text'].apply(lambda x:len(str(x).split()))
 
     print("Removing All Stop Words")
     df['desc_clean'] = df['text'].apply(clean_txt)
-    # df = pd.read_csv(r'C:\Users\elton\Desktop\Kate_Project\code\cleaned_text.csv')
+    # df = pd.read_csv(r'C:\Users\yunxinliu\Documents\GitHub\cleaned_text.csv')
     # print(df.head())
     df.set_index('name',inplace = True)
     print(df.head())
@@ -126,7 +122,27 @@ def load_model():
     return cosine_similarity, indices, df, seattleHotels
 
 
-
+def turn_class_into_star(hotelInfo):
+    hotelclass=int(hotelInfo)
+    if hotelclass == 1:
+        st.write("**_Hotel Class_**: ★☆☆☆☆")
+    if hotelclass == 2:
+        st.write("**_Hotel Class_**: ★★☆☆☆")
+    if hotelclass == 3:
+        st.write("**_Hotel Class_**: ★★★☆☆")
+    if hotelclass == 4:
+        st.write("**_Hotel Class_**: ★★★★☆")
+    if hotelclass == 5:
+        st.write("**_Hotel Class_**: ★★★★★")
+        
+def turn_cleanliness_into_mean(hotelInfo):
+    cleanliness = float(hotelInfo)
+    st.write("**_Cleanliness Rating_**:", "{:.2f}".format(cleanliness))
+    
+def turn_sleep_into_mean(hotelInfo):
+    sleepquality = float(hotelInfo)
+    st.write("**_Sleep Comfort Rating_**:", "{:.2f}".format(sleepquality))
+        
 if __name__ == '__main__':
     # LOAD MODEL
     cosine_similarity, indices, df, seattleHotels= load_model()
@@ -134,8 +150,11 @@ if __name__ == '__main__':
     st.title("New York & Seattle Similar Hotel Recommender App")
     st.subheader('Overview')
     st.markdown("Author: Yunxin Liu")
-    st.markdown("A hotel recommendation system that will make use of cosine similarity to output similar recommendations based on features")
-    st.markdown("The dataset came from https://www.cs.cmu.edu/~jiweil/html/hotel-review.html. The author crawled 878,561 reviews from 4,333 hotels in more than 10 states through TripAdvisor")
+    st.markdown("A hotel recommendation system that will make use of cosine similarity to output similar recommendations in New York based on selected features from a Seattle's hotel.")
+
+    st.markdown("""<hr style="height:5px;border:none;color:#333;background-color:#989797;" /> """, unsafe_allow_html=True)
+    st.markdown("Hit the **_Start my search_**🔎 button on the sidebar to pick your favorite hotels in New York!")
+    st.text("______________________________________________________________________________________________________________________________________________________________________________________")
 
     with st.sidebar.form(key="Form1"):
         with st.sidebar:
@@ -165,11 +184,11 @@ if __name__ == '__main__':
                 hotel_classes.append(1) 
             st.sidebar.text("")
             st.sidebar.text("")
-            st.sidebar.markdown('**_Optional_**: Preferred range of cleanliness')
+            st.sidebar.markdown('**_Optional_**: Preferred range of cleanliness (Select a range)')
             clean=st.sidebar.slider("Cleanliness level",value=(1,1),max_value=5)
             st.sidebar.text("")
             st.sidebar.text("")
-            st.sidebar.markdown('**_Optional_**: Preferred range of sleep comfort level')
+            st.sidebar.markdown('**_Optional_**: Preferred range of sleep comfort level (Select a range)')
             sleep=st.sidebar.slider("Sleep quality",value=(1,1),max_value=5)
             st.sidebar.text("")
             st.sidebar.text("")       
@@ -177,16 +196,28 @@ if __name__ == '__main__':
     if (submitted1):
         if len(hotelName) == 0:
             print("Hotel Name Not Entered")
-            st.write("Please Enter Hotel Name")
+            st.write("Please Enter Hotel Name in Seattle")
         else:
-            st.write("\nTop 10 Similar Hotels in New York:")
+            st.write("\n**_Top 10 Similar Hotels in New York based on your selections_**:")
             # print(hotelName)
             # print(indices.head())
-            print(clean)
+            #print(clean)
             hotelList = recommendations(hotelName, hotel_classes, clean, sleep, cosine_similarity, indices, df)
             resultpd = pd.DataFrame(hotelList)
-            st.table(resultpd)
+            #st.table(resultpd)
+            i = 1
             for hotelInfo in hotelList:
-                print(hotelInfo)
-
-        
+                st.write("**_Reccommendation_**",i)  
+                st.write("**_Hotel Name_**:", hotelInfo[0] , "\n")
+                st.write(hotelInfo[1] , "\n")
+                st.write("**_Address_**:",hotelInfo[3],"\n")
+                turn_class_into_star(hotelInfo[7])
+                turn_cleanliness_into_mean(hotelInfo[8])
+                turn_sleep_into_mean(hotelInfo[9])
+                st.text("______________________________________________________________________________________________________________________________________________________________________________________")
+                i+=1
+    st.sidebar.text("")
+    st.sidebar.text("")   
+    st.sidebar.text("")
+    st.sidebar.text("") 
+    st.markdown("The dataset came from https://www.cs.cmu.edu/~jiweil/html/hotel-review.html. The author crawled 878,561 reviews from 4,333 hotels in more than 10 states through TripAdvisor. This app used the data from New York and Seattle to speed up the runtime.")
