d7cd6498f54a2d60a1146a9848f91c567b365293
YXLIU926 <93688200+YXLIU926@users.noreply.github.com>
2021-12-06 11:40:51 -0600

    Enter a hotel name and other features to begin your search

--- app.py
+++ app.py
@@ -10,78 +10,25 @@ import cufflinks
 from plotly.offline import iplot
 import json
 import streamlit as st
-
-##############################Streamlit Appp###################################
-st.title("New York & Seattle Similar Hotel Recommender App")
-st.subheader('Overview')
-st.markdown("Author: Yunxin Liu")
-st.markdown("A hotel recommendation system that will make use of cosine similarity to output similar recommendations based on features")
-st.markdown("The dataset came from https://www.cs.cmu.edu/~jiweil/html/hotel-review.html. The author crawled 878,561 reviews from 4,333 hotels in more than 10 states through TripAdvisor")
-
-with st.sidebar.form(key="Form1"):
-    with st.sidebar:
-        feature = []
-        st.sidebar.markdown("Enter a hotel name that you enjoyed your stay in Seattle. This app will fetch 10 similar hotels in New York for you")
-        user_input=st.sidebar.text_input("")
-        feature = [str(user_input).lower()]
-        st.sidebar.text("")
-        st.sidebar.text("")
-        st.sidebar.markdown('**_Optional_**: Filter the hotels by ratings')
-        five_star = st.sidebar.checkbox('★★★★★')    
-        four_star = st.sidebar.checkbox('★★★★☆')
-        three_star = st.sidebar.checkbox('★★★☆☆')
-        two_star = st.sidebar.checkbox('★★☆☆☆')
-        one_star = st.sidebar.checkbox('★☆☆☆☆')    
-        feature_star = []
-        if (five_star):
-            feature_star.append(5)
-        if (four_star): 
-            feature_star.append(4)
-        if (three_star):
-            feature_star.append(3)
-        if (two_star):
-            feature_star.append(2)
-        if (one_star):
-            feature_star.append(1) 
-        st.sidebar.text("")
-        st.sidebar.text("")
-        st.sidebar.markdown('**_Optional_**: Preferred range of cleanliness')
-        clean=st.sidebar.slider("Cleanliness level",value=(1,1),max_value=5)
-        st.sidebar.text("")
-        st.sidebar.text("")
-        st.sidebar.markdown('**_Optional_**: Preferred range of sleep comfort level')
-        sleep=st.sidebar.slider("Sleep quality",value=(1,1),max_value=5)
-        st.sidebar.text("")
-        st.sidebar.text("")       
-        submitted1 = st.form_submit_button(label = 'Start my search!🔎')
-
 nltk.download('stopwords')
 
-# GLOBAL VAIRABLES THAT WILL BE USED IN TEXT CLEAN FUNCTIOn
+
+# GLOBAL VAIRABLES THAT WILL BE USED IN TEXT CLEAN FUNCTION & MODEL BUILD
 stop_words = ""
 sub_replace = None
 
-#########################################################################
-@st.cache
-def dataInfo(df):
-    # HOTEL DESCRIPTION TOTAL WORDS
-    vec = CountVectorizer().fit(df['desc'])
-    bag_of_words = vec.transform(df['desc'])
-    bag_of_words.toarray()
-    print(bag_of_words.shape)
-    # HOTEL DESCRIPTION TOTAL WORDS
-    sum_words = bag_of_words.sum(axis=0)
-    print(sum_words)
 
-    # DESCRIPTION FREQUNCIES
-    words_freq = [(word,sum_words[0,idx]) for word,idx in vec.vocabulary_.items()]
-    print(words_freq)
-    # DESCRIPTION FREQUNCIES IN ORDER
-    words_freq = sorted(words_freq,key=lambda x:x[1],reverse=True)
-    print(words_freq)
+
+def clean_txt(text):
+    text.lower()
+    text = sub_replace.sub('',text)
+    ' '.join(word for word in text.split() if word not in stop_words)
+    return text
+
+
+#########################################################################
 
 # TOP FREQUNCY WORDS
-@st.cache
 def get_top_n_words(corpus,n=None):
     vec = CountVectorizer(stop_words='english',ngram_range=(1,3)).fit(corpus)
     bag_of_words = vec.transform(corpus)
@@ -89,21 +36,21 @@ def get_top_n_words(corpus,n=None):
     words_freq = [(word,sum_words[0,idx]) for word,idx in vec.vocabulary_.items()]
     words_freq = sorted(words_freq,key=lambda x:x[1],reverse=True)
     return words_freq[:n]
-@st.cache
-def clean_txt(text):
-    text.lower()
-    text = sub_replace.sub('',text)
-    ' '.join(word for word in text.split() if word not in stop_words)
-    return text
 
-@st.cache
-def recommendations(name,cosine_similarity, indices, df):
+
+def recommendations(name, hotel_class_required, cleanliness_required, sleep_quality_required, cosine_similarity, indices, df):
     recommended_hotels = []
     idx = indices[indices == name].index[0]
-    print(idx)
     score_series = pd.Series(cosine_similarity[idx]).sort_values(ascending=False)
     top_10_indexes = list(score_series[1:1000].index)
     count = 0
+    hotel_class_boolean = False
+    tempList = []
+    if len(hotel_class_required) > 0:
+        hotel_class_boolean = True
+        for hotelClass in hotel_class_required:
+            tempList.append(int(hotelClass))
+        hotel_class_required = tempList
     for i in top_10_indexes:
         hotelName = list(df.index)[i]
         url = list(df.url)[i]
@@ -111,8 +58,27 @@ def recommendations(name,cosine_similarity, indices, df):
         street = list(df.street_address)[i]
         region = list(df.region)[i]
         city = list(df.city)[i]
+        if (city != "New York City"):
+            continue
         postal_code = list(df.postal_code)[i]
-        hotelInfo = [hotelName, url, phone, street, region, city, postal_code]
+        hotel_class = int(list(df.hotel_class)[i])
+        if hotel_class_boolean and hotel_class not in hotel_class_required:
+            continue
+        cleanliness = float(list(df.cleanliness)[i])
+        # USER SET CLEANLINESS REQUIREMENT
+        if (cleanliness_required[0] != cleanliness_required[1]):
+            lowerBound = cleanliness_required[0]
+            higherBound = cleanliness_required[1]
+            if cleanliness < lowerBound or cleanliness > higherBound:
+                continue
+        sleep_quality = float(list(df.sleep_quality)[i]) 
+        # UsER SET SLEEP QUALITY REQUIREMENT
+        if (sleep_quality_required[0] != sleep_quality_required[1]):
+            lowerBound = sleep_quality_required[0]
+            higherBound = sleep_quality_required[1]
+            if sleep_quality < lowerBound or sleep_quality > higherBound:
+                continue
+        hotelInfo = [hotelName, url, phone, street, region, city, postal_code, str(hotel_class), str(cleanliness), str(sleep_quality)]
         # recommended_hotels.append(list(df.index)[i])
         recommended_hotels.append(hotelInfo)
         count += 1
@@ -120,32 +86,34 @@ def recommendations(name,cosine_similarity, indices, df):
             break
     return recommended_hotels
 
-@st.cache
-def test():
-    global sub_replace, stop_words
-    # print(1)
-    # print(stopwords)
-    # LOAD DATA
-    print("Loading Data")
-    df = pd.read_csv(r'C:\Users\yunxinliu\Documents\GitHub\clean_data.csv')
-    # RPINT SOME DATA INFORMATION
-    # dataInfo(df)
-    # GET MOST COMMON WORDS FROM DESCRIPTION
-    # common_words=get_top_n_words(df['desc'],20)
-    # df3 = pd.DataFrame(common_words,columns=['desc','count'])
-    # df3.groupby('desc').sum()['count'].sort_values().iplot(kind='barh',yTitle='Count',linecolor='black',title='top 20 before remove stopwords-ngram_range=(2,2)')
-    print("Calculating Word Total For Each Review")
-    df['word_count']=df['text'].apply(lambda x:len(str(x).split()))
-    # print(df.head())
-    # df1.groupby('desc').sum()['count'].sort_values().iplot(kind='barh',yTitle='Count',linecolor='black',title='top 20 before remove stopwords')
 
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    global stop_words
+    global sub_replace
+    seattleHotels = []
+    #generateCleanDataCSV()
     sub_replace = re.compile('[^0-9a-z #+_]')
     stop_words = set(stopwords.words('english'))
+    print("Loading Data")
+    df = pd.read_csv(r'C:\Users\elton\Desktop\Kate_Project\code\clean_data.csv')
+    print(df.head())
+    tempdf = df[['name', 'city']]
+    for index, row in tempdf.iterrows():
+        if row['city'] == "Seattle":
+            seattleHotels.append(row['name'])
+    seattleHotels = seattleHotels.sort()
+
+    print("Calculating Word Total For Each Review")
+    df['word_count']=df['text'].apply(lambda x:len(str(x).split()))
+
     print("Removing All Stop Words")
     df['desc_clean'] = df['text'].apply(clean_txt)
+    # df = pd.read_csv(r'C:\Users\elton\Desktop\Kate_Project\code\cleaned_text.csv')
     # print(df.head())
-    
     df.set_index('name',inplace = True)
+    print(df.head())
     tf=TfidfVectorizer(analyzer='word',ngram_range=(1,3),stop_words='english')
     tfidf_matrix=tf.fit_transform(df['desc_clean'])
     print(tfidf_matrix.shape)
@@ -154,124 +122,71 @@ def test():
     print(cosine_similarity.shape)
     # print(cosine_similarity[0])
     indices = pd.Series(df.index)
-    print(indices[:5])
-    print("Now Recommending")
-    # return cosine_similarity
-    print(recommendations('The Michelangelo Hotel',cosine_similarity, indices, df))
-@st.cache
-def readHotels():
-    # dd = pd.read_json('review.txt')
-    contents = []
-    with open(r"C:\Users\yunxinliu\Documents\GitHub\hotels.txt") as f:
-        for line in f:
-            contents.append(json.loads(line))
-    format_rows = []
-    for row in contents:
-        # print(row)
-        # break
-        city = row["address"]["locality"]
-        if (city != "New York City" and city != "Seattle"):
-            continue
-        id = row["id"]
-        name = row["name"]
-        if ("hotel_class" in row):
-            hotel_class = row["hotel_class"]
+    print("Finished ")
+    return cosine_similarity, indices, df, seattleHotels
+
+
+
+if __name__ == '__main__':
+    # LOAD MODEL
+    cosine_similarity, indices, df, seattleHotels= load_model()
+    ##############################Streamlit Appp###################################
+    st.title("New York & Seattle Similar Hotel Recommender App")
+    st.subheader('Overview')
+    st.markdown("Author: Yunxin Liu")
+    st.markdown("A hotel recommendation system that will make use of cosine similarity to output similar recommendations based on features")
+    st.markdown("The dataset came from https://www.cs.cmu.edu/~jiweil/html/hotel-review.html. The author crawled 878,561 reviews from 4,333 hotels in more than 10 states through TripAdvisor")
+
+    with st.sidebar.form(key="Form1"):
+        with st.sidebar:
+            st.sidebar.markdown("Enter a hotel name that you enjoyed your stay in Seattle. This app will fetch 10 similar hotels in New York for you")
+            # Host_Country = st.selectbox('Select HomeTeamName name:',('France', 'Spain', 'Italy', 'England', 'Belgium', 'Portugal','Sweden'))
+
+            user_input=st.sidebar.selectbox('',tuple(seattleHotels))
+            hotelName = str(user_input)
+            st.sidebar.text("")
+            st.sidebar.text("")
+            st.sidebar.markdown('**_Optional_**: Filter the hotels by ratings')
+            five_star = st.sidebar.checkbox('★★★★★')    
+            four_star = st.sidebar.checkbox('★★★★☆')
+            three_star = st.sidebar.checkbox('★★★☆☆')
+            two_star = st.sidebar.checkbox('★★☆☆☆')
+            one_star = st.sidebar.checkbox('★☆☆☆☆')    
+            hotel_classes = []
+            if (five_star):
+                hotel_classes.append(5)
+            if (four_star): 
+                hotel_classes.append(4)
+            if (three_star):
+                hotel_classes.append(3)
+            if (two_star):
+                hotel_classes.append(2)
+            if (one_star):
+                hotel_classes.append(1) 
+            st.sidebar.text("")
+            st.sidebar.text("")
+            st.sidebar.markdown('**_Optional_**: Preferred range of cleanliness')
+            clean=st.sidebar.slider("Cleanliness level",value=(1,1),max_value=5)
+            st.sidebar.text("")
+            st.sidebar.text("")
+            st.sidebar.markdown('**_Optional_**: Preferred range of sleep comfort level')
+            sleep=st.sidebar.slider("Sleep quality",value=(1,1),max_value=5)
+            st.sidebar.text("")
+            st.sidebar.text("")       
+            submitted1 = st.form_submit_button(label = 'Start my search!🔎')
+    if (submitted1):
+        if len(hotelName) == 0:
+            print("Hotel Name Not Entered")
+            st.write("Please Enter Hotel Name")
         else:
-            hotel_class = 0.0
-        url = row["url"]
-        phone = row["phone"]
-        region = row["address"]["region"]
-        if "street-address" in row["address"]:
-            street_address = row["address"]["street-address"]
-        else:
-            street_address = ""
-        if "postal-code" in row["address"]:
-            postal_code = row["address"]["postal-code"]
-        else:
-            postal_code = 00000
+            st.write("\nTop 10 Similar Hotels in New York:")
+            # print(hotelName)
+            # print(indices.head())
+            print(clean)
+            hotelList = recommendations(hotelName, hotel_classes, clean, sleep, cosine_similarity, indices, df)
+            resultpd = pd.DataFrame(hotelList)
+            st.table(resultpd)
+            for hotelInfo in hotelList:
+                print(hotelInfo)
+
         
-        format_rows.append([id, name, hotel_class, url, phone, region, street_address, postal_code, city])
-    data = pd.DataFrame(format_rows)
-    data.columns = ['id', 'name', 'hotel_class', 'url', 'phone', 'region', 'street_address', 'postal_code', 'city']
-    return data
-@st.cache
-def readReviews():
-    # dd = pd.read_json('review.txt')
-    contents = []
-    dict = {}
-    with open(r"C:\Users\yunxinliu\Documents\GitHub\review.txt") as f:
-        for line in f:
-            contents.append(json.loads(line))
-    format_rows = []
-    for row in contents:
-        id = row["offering_id"]
-        if "service" in  row["ratings"]:
-            service = row["ratings"]["service"]
-        else:
-            service = 0.0
-        if "cleanliness" in  row["ratings"]:
-            cleanliness = row["ratings"]["cleanliness"]
-        else:
-            cleanliness = 0.0
-        if "location" in  row["ratings"]:
-            location = row["ratings"]["location"]
-        else:
-            location = 0.0
-        if "sleep_quality" in  row["ratings"]:
-            sleep_quality = row["ratings"]["sleep_quality"]
-        else:
-            sleep_quality = 0.0
-        if "rooms" in  row["ratings"]:
-            rooms = row["ratings"]["rooms"]
-        else:
-            rooms = 0.0
-        text = row["text"]
-        if (id in dict):
-            dict[id].append([id, service, cleanliness, location, sleep_quality, rooms, text])
-        else:
-            dict[id] = [[id, service, cleanliness, location, sleep_quality, rooms, text]]
-    # for k,v in dict.items():
-    #     print(k)
-    #     print(v)
-    #     break   
-    for k,v in dict.items():
-        id = k
-        text = ""
-        service = 0.0
-        cleanliness = 0.0
-        location = 0.0
-        sleep_quality = 0.0
-        rooms = 0.0
-        count = 0
-        for innerList in v:
-            service += float(innerList[1])
-            cleanliness += float(innerList[2])
-            location += float(innerList[3])
-            sleep_quality += float(innerList[4])
-            rooms += float(innerList[5])
-            text += innerList[6]
-            count += 1
-        service /= count
-        cleanliness /= count
-        location /= count
-        sleep_quality /= count
-        rooms /= count
-        format_rows.append([id, service, cleanliness, location, sleep_quality, rooms, text])
-    data = pd.DataFrame(format_rows)
-    data.columns = ['id', 'service', 'cleanliness', 'location', 'sleep_quality', 'rooms', 'text']
-    return data
-
-@st.cache
-def generateCleanDataCSV():
-    hotel_df = readHotels()
-    print(hotel_df.head())
-    review_df = readReviews()
-    print(review_df.head())
-    print(len(review_df))
-    final_df = pd.merge(hotel_df, review_df, on="id")
-    final_df.to_csv(r'C:\Users\yunxinliu\Documents\GitHub\clean_data.csv', index = False)
-    print(len(final_df))
-
-#generateCleanDataCSV()
-st.write("\nTop 10 Similar Hotels in New York:")
-
